9a47279fef3c653a1b68bce3e73d0e4e1e2ad6f0
Cristina Romero Mirete <crm128@gcloud.ua.es>
2024-05-04 18:38:57 +0200

    added feature extraction and updated README

---
+++ Preprocessing/Feature_Extraction.m
@@ -0,0 +1,187 @@
+clear;
+close;
+clc;
+
+users = {'H001B0101', 'H002B0101', 'H003B0101', 'H004B0101', 'H022B0101', 'H023B0101', 'H024B0101', 'H031B0101', 'H032B0101'};
+
+window_size = 2500; % Define window size (adjust as needed)
+overlap = 500; % Define overlap between windows (adjust as needed)
+
+for i = 1:length(users)
+    % Access the current user
+    user = users{i};
+    disp(['Feature extraction for user: ', user]);
+    load(['results/' user '/' 'emg_and_force_runs_' user '.mat']);
+    
+    for exp_index=2:4   
+        % MAV COMPUTATION -----------------------------------------------------
+        run = emg_runs{exp_index, 1};
+        time_run = emg_runs{exp_index, 2};
+
+        % Rectify the signal
+        rectified_run = abs(run);
+        
+         % Compute linear envelope for each channel
+        linear_envelope = zeros(size(rectified_run));
+        for channel = 1:size(rectified_run, 2)
+            linear_envelope(:, channel) = smooth(rectified_run(:, channel), 2000); % Smoothing can be adjusted as needed
+        end
+        
+        % Write preprocessed data to CSV
+        csvwrite(['results2/' user '/' 'emg_experiment_' num2str(exp_index) '_mav.csv'], linear_envelope);
+        
+        % MNF COMPUTATION -----------------------------------------------------
+        % Load the preprocessed HD-EMG data from the CSV file
+        filename = ['results2/' user '/emg_experiment_' num2str(exp_index) '_mav.csv'];
+        hd_emg_data = csvread(filename); 
+    
+        % Compute number of windows
+        num_windows = floor((size(hd_emg_data, 1) - window_size) / (window_size - overlap)) + 1;
+
+        % Initialize array to store mean frequencies for each channel and each window
+        mnf_values = zeros(num_windows, size(hd_emg_data, 2));
+
+        Fs = 2000;
+
+        % Iterate over each window
+        for i = 1:num_windows
+            % Determine indices for current window
+            start_index = (i - 1) * (window_size - overlap) + 1;
+            end_index = start_index + window_size - 1;
+
+            % Extract data for current window
+            window_data = hd_emg_data(start_index:end_index, :);
+
+            % Compute power spectral density (PSD) using periodogram for each channel
+            [Pxx, f] = periodogram(window_data, [], [], Fs, 'power');
+
+            % Compute Mean Frequency (MNF) for each channel in the current window
+            mnf_values(i, :) = meanfreq(Pxx, f);
+            
+            % Check for NaN values and replace them with 0
+            nan_indices = isnan(mnf_values(i, :));
+            mnf_values(i, nan_indices) = 0;
+        end
+        
+        % Write feature data to CSV
+        csvwrite(['results2/' user '/' 'emg_experiment_' num2str(exp_index) '_mnf.csv'], mnf_values);
+        
+        
+        % RMS COMPUTATION -----------------------------------------------------
+        hd_emg_data = emg_runs{exp_index, 1};
+
+        % Rectify the signal
+        rectified_data = abs(hd_emg_data);
+        
+        % Compute number of windows
+        num_windows = floor((size(rectified_data, 1) - window_size) / (window_size - overlap)) + 1;
+
+        % Initialize array to store mean frequencies for each channel and each window
+        rms_values = zeros(num_windows, size(rectified_data, 2));
+
+        Fs = 2000;
+
+        % Iterate over each window
+        for i = 1:num_windows
+            % Determine indices for current window
+            start_index = (i - 1) * (window_size - overlap) + 1;
+            end_index = start_index + window_size - 1;
+
+            % Extract data for current window
+            window_data = rectified_data(start_index:end_index, :);
+
+            % Compute Root Mean Square (RMS) for each channel in the current window
+            rms_values(i, :) = rms(window_data);
+        end
+        
+        % Write feature data to CSV
+        csvwrite(['results2/' user '/' 'emg_experiment_' num2str(exp_index) '_rms.csv'], rms_values);
+        
+    end
+      
+end
+
+%% PLOT RESULTS
+
+% Define the number of rows and columns for the grid
+num_rows = 4;
+num_columns = 4;
+
+% Create vector of window indices
+window_indices = (1:num_windows) * (window_size - overlap) - overlap / 2;
+
+
+% MAV SHOWING -----------------------------------------------------
+% Create a new figure
+figure;
+% Iterate over each channel and plot the RMS values in a subplot
+for i = 1:size(linear_envelope, 2)/4
+    % Determine the subplot position
+    subplot(num_rows, num_columns, i);
+    
+    % Plot the RMS values for the current channel
+    plot(linear_envelope(:, i), 'b', 'LineWidth', 1);
+    
+    % Set subplot title
+    title(['Channel ', num2str(i)]);
+    
+    % Set subplot labels
+    xlabel('Time');
+    ylabel('Amplitude');
+    
+    % Set grid
+    grid on;
+end
+
+% Adjust figure properties
+sgtitle('MAV for the first 16 channels');
+
+% MNF SHOWING -----------------------------------------------------
+% Create a new figure
+figure;
+% Iterate over each channel and plot the RMS values in a subplot
+for i = 1:size(mnf_values, 2)/4
+    % Determine the subplot position
+    subplot(num_rows, num_columns, i);
+    
+    % Plot the RMS values for the current channel
+    plot(window_indices, mnf_values(:, i), 'b', 'LineWidth', 1);
+    
+    % Set subplot title
+    title(['Channel ', num2str(i)]);
+    
+    % Set subplot labels
+    xlabel('Window index');
+    ylabel('Amplitude');
+    
+    % Set grid
+    grid on;
+end
+
+% Adjust figure properties
+sgtitle('MNF for the first 16 channels');
+
+% RMS SHOWING -----------------------------------------------------
+% Create a new figure
+figure;
+% Iterate over each channel and plot the RMS values in a subplot
+for i = 1:size(rms_values, 2)/4
+    % Determine the subplot position
+    subplot(num_rows, num_columns, i);
+    
+    % Plot the RMS values for the current channel
+    plot(window_indices, rms_values(:, i), 'b', 'LineWidth', 1);
+    
+    % Set subplot title
+    title(['Channel ', num2str(i)]);
+    
+    % Set subplot labels
+    xlabel('Window index');
+    ylabel('Amplitude');
+    
+    % Set grid
+    grid on;
+end
+
+% Adjust figure properties
+sgtitle('RMS for the first 16 channels');
--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-# HD-EMG Fatigue estimation
-This repository stores all the code implemented for the HD-EMG Fatigue experimental setup and estimation.
+# HD-EMG Fatigue proportional estimation
+This repository stores all the code implemented for my master's thesis in Artificial Intelligence, including the HD-EMG Fatigue experimental setup, preprocessing and proportional estimation via various AI models.
 
-## Experiment Division
-All the MATLAB code uploaded corresponds to the experiment division task, where each user HD-EMG and Force data is separated accordingly to the four trials/runs made.
+## 📥Download data
+In order to work with the original HD-EMG data obtained and preprocessed, this Google Drive folder must be cloned: 
+https://drive.google.com/drive/folders/1kRsAC32kRw5VUpvQzqezt9Unv489gPIW?usp=sharing
+
+## 📈Preprocess data
+
+The data preprocessing phase, stored in the `Preprocessing` folder, is mainly divided in two steps: **experiment division** of the original recorded data in order to obtain the 4 runs segmented, and MAV, RMS and MNF **feature extraction** of these signals.
+
+###  Experiment Division
+In this step, each user HD-EMG and Force data is separated accordingly to the four trials/runs made.
 
 The tasks performed by each file are described below:
 
@@ -19,3 +27,22 @@ The tasks performed by each file are described below:
   - **Output parameters**: MATLAB cell arrays containing the divided HD-EMG and Force runs, along with their respective Time data after cutting.
     
 - `main_experiment_division.m`: reads HD-EMG and Force data for a specific user and divides the data into separate experimental runs using the auxiliary functions defined in the previous files, plots the results and lastly saves the experimental runs in `.mat` and `.csv` files.
+
+### Feature extraction
+
+In this step, for each of the HD-EMG experimental runs, three features are extracted: Mean Absolute Value (MAV), Root Mean Square (RMS), and Mean Frequency (MNF). These features provide insights into the muscle activity and can be used for further analysis and modeling.
+
+The tasks performed for feature extraction are described below:
+
+- **MAV Computation**: Mean Absolute Value (MAV) is obtained for each channel of the HD-EMG signal. First, the signal is rectified by taking the absolute value, then a linear envelope is computed using moving average filter smoothing.
+
+- **MNF Computation**: Mean Frequency (MNF) is computed for each channel of the HD-EMG signal using the Power Spectral Density (PSD) obtained from the periodogram. The signal is divided into windows, and for each window, the PSD is computed and used to calculate the MNF. 
+
+- **RMS Computation**: Root Mean Square (RMS) is computed for each channel of the rectified HD-EMG signal. The signal is divided into windows, and for each window, the RMS is computed. 
+
+These feature extraction processes are repeated for each experimental run of each user, resulting in its corresponding CSV files that contain feature matrices of MAV, RMS, and MNF values.
+
+## 🛠️Models
+
+## 📏Results
+
